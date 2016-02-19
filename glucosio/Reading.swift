025982466d9c7aea1c54f96f2c9386a0a77b3a3e
Eugenio Baglieri <baglieri.eugenio@gmail.com>
2016-02-19 20:16:06 +0100

    Begin to work on model
    Model file added to project
    Entities created

---
+++ glucosio/Reading.swift
@@ -0,0 +1,17 @@
+//
+//  Reading.swift
+//  glucosio
+//
+//  Created by Eugenio Baglieri on 17/02/16.
+//  Copyright © 2016 Eugenio Baglieri. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Reading: ModelObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
